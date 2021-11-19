4980a051873608b35598a4509d428f1b9906727f
ioan <55089021+IOANSTIRBU@users.noreply.github.com>
2021-11-19 09:41:16 +0100

    BaseTest added

---
+++ src/test/java/com/example/selenium/WebPagesTest/demoqa/BaseTest.java
@@ -0,0 +1,36 @@
+package com.example.selenium.WebPagesTest.demoqa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class BaseTest {
+
+    //Driver del navegador
+    WebDriver webdriver;
+    JavascriptExecutor js;
+
+    @BeforeEach
+    void setup() {
+        //esta es la ruta del proyecto
+        String dir = System.getProperty("user.dir");
+        String driverUrl = "/drivers/operadriver.exe";
+        String url = dir + driverUrl;
+        System.setProperty("webdriver.opera.driver", url);
+        //Driver de Navegador Opera
+        webdriver = new OperaDriver();
+        js = (JavascriptExecutor) webdriver;
+        webdriver.manage().window().maximize();
+    }
+
+    //Esto es para que se cierre el navegador una vez completado el test
+    @AfterEach
+    void tearDown() {
+        webdriver.quit();
+
+    }
+
+
+}
